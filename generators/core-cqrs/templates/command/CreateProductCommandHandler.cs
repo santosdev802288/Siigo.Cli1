1a77f6c6e53ed6ce691d224dba1472e09614c1de
Juan Correa <juancorrea@192.168.0.11>
2020-12-09 14:36:54 -0500

    feat(command): add core-cqrs commando to generate commands and queries for .net core microservices

---
+++ generators/core-cqrs/templates/command/CreateProductCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace <%= config.name %>.Application.Commands
+{
+    /// <summary>
+    /// Handler which processes the command when
+    /// customer executes cancel order from app
+    /// </summary>
+    public class <%= config.command_name %>CommandHandler : IRequestHandler<<%= config.command_name %>Command, int>
+    {
+        // Add any injected repository/helper/util 
+        // or any class needed to handle the command
+        public <%= config.command_name %>CommandHandler()
+        {
+        }
+
+        public async Task<int> Handle(<%= config.command_name %>Command request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(1);
+        }
+    }
+}
