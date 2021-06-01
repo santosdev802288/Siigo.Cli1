084445adb4958a093dc0375507adc33047db5dd1
Juan Navarro <juan.navarro@siigo.com>
2021-06-01 15:21:28 -0500

    feat: show available updates for generator-siigo
    Force the update on a major version release

--- utils/notification.js
+++ utils/notification.js
@@ -1,8 +1,20 @@
 const updateNotifier = require('update-notifier');
-const pkg = require('../package');
+const pkg = require('../package.json');
 
 function verifyNewVersion() {
-    updateNotifier({pkg}).notify();
+    // Checks for available update and returns an instance
+    const notifier = updateNotifier({
+        pkg,
+        updateCheckInterval: 1000 * 60 * 60 * 24 * 7 // 1 week
+    });
+
+    // Notify using the built-in convenience method
+    notifier.notify({isGlobal: true});
+
+    // Stop if a major version is available
+    if (notifier.update?.type === 'major') {
+        throw new Error(`Update to the last version of ${pkg.name}`)
+    }
 }
 
 module.exports = verifyNewVersion;
