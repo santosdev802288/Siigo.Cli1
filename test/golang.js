0698657f50fc6f97145df02f71ea85cf833e1e33
Juan Sebastian Navarro Avila <juan.navarro@siigo.com>
2021-06-21 17:27:09 -0500

    test: add mocha test for golang generator

---
+++ test/golang.js
@@ -0,0 +1,22 @@
+const assert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
+
+const MyGenerator = require('../generators/bolt');
+
+describe('siigo:bolt', () => {
+    it('Generates a Go project', () => {
+        const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'Siigo.Microservice.Bolt'))
+
+        return helpers.run(MyGenerator, {resolved: path.join(__dirname, '../generators/bolt/index.js'), namespace: 'siigo:golang'})
+            .inDir(dir)
+            .withOptions({ 'personal-token': 'mytoken' })      // Mock options passed in
+            .withPrompts({ ready: true })   // Mock the prompt answers
+            .then(() => {
+                // assert something about the generator
+                assert.ok('.gitignore');
+            });
+    });
+});
