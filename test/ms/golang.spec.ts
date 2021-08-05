f82bed5124f549ca3fe5b27a8debdff24d37a594
Francisco Javier Anacona Campo <anac800776@siigo.com>
2021-08-04 21:05:16 -0500

    test: :lock: update testo golang

--- test/ms/golang.spec.ts
+++ test/ms/golang.spec.ts
@@ -37,8 +37,8 @@ describe(NAMESPACE, () => {
 
         return helpers.run(GolangMSGenerator, {resolved: path.join(__dirname, GENERATOR_FOLDER, 'index.js'), namespace: NAMESPACE})
             .inDir(dir)
-            .withOptions({ 'personal-token': 'myToken', 'project-name':  name})
-            .withPrompts({ ready: true, prefix: folderPrefix })   // Mock the prompt answers
+            .withOptions({ 'personal-token': 'myToken', 'project-name': name})
+            .withPrompts({ ready: true, prefix: folderPrefix, name:  name })   // Mock the prompt answers
             .then(() => {
                 assert.ok(process.cwd().endsWith(`${folderPrefix}${name}`))
                 // assert something about the generator
@@ -54,11 +54,11 @@ describe(NAMESPACE, () => {
 
         const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'Golang'))
         const folderPrefix = 'Siigo.Microservice.'
-
+        const name = 'NoFolder'
         return helpers.run(GolangMSGenerator, {resolved: path.join(__dirname, GENERATOR_FOLDER, 'index.js'), namespace: NAMESPACE})
             .inDir(dir)
             .withOptions({ })
-            .withPrompts({ ready: true, prefix: folderPrefix })   // Mock the prompt answers
+            .withPrompts({ ready: true, prefix: folderPrefix, name: name })   // Mock the prompt answers
             .then(() => {
                 assert.fail()
             })
