fcf5481e89a03e6196865a01d35a57a7d6399002
Juan Sebastian Navarro Avila <juan.navarro@siigo.com>
2021-08-17 16:12:48 -0500

    chore: reduce update interval and change release config file

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -11,7 +11,7 @@ export function verifyNewVersion(): void {
     // Checks for available update and returns an instance
     const notifier = updateNotifier({
         pkg,
-        updateCheckInterval: 1000 * 60 // 1 minute
+        updateCheckInterval: 1000 * 1 // 1 second
     });
 
     // Notify using the built-in convenience method
