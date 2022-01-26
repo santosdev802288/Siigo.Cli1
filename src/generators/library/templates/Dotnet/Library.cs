1cac1e808280e7bb17637163fa29a53599754ae3
Francisco Anacona <anac800776>
2022-01-26 10:11:25 -0500

    add dotnet module and library option

---
+++ src/generators/library/templates/Dotnet/Library.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Siigo.Core.<%= config.nameCapitalize %>
+{
+    public class <%= config.nameCapitalize %>
+    {
+        public static void About()
+        {
+            Console.WriteLine("Siigo <%= config.nameCapitalize %> library");
+        }
+    }
+}
