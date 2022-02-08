1151d55090c2c855cb329c5b54592eeea380ca22
Juan Sebastian Navarro Avila <juan.navarro@siigo.com>
2022-02-08 17:55:39 -0500

    test(test-ak6): check if test is running on Azure DevOps

--- test/test/ak6.spec.ts
+++ test/test/ak6.spec.ts
@@ -4,9 +4,13 @@ import fs from 'fs'
 import path from 'path'
 import os from 'os'
 import shelljs from 'shelljs'
+import sinon from 'sinon'
 
 import { getGenerator, SiigoGenerator } from '../generator.factory'
 import { TestingPrefix } from '../../src/utils/generator/testing'
+import { runningOnAzurePipeline } from '../../src/utils/azureDevops'
+import * as gitmanager from '../../src/utils/gitmanager'
+import * as siigoFile from '../../src/utils/siigoFile'
 
 enum YARN_AUDIT_CODE{
   INFO = 1,
@@ -19,6 +23,14 @@ enum YARN_AUDIT_CODE{
 describe('siigo:test.ak6', () => {
   const generator = getGenerator(SiigoGenerator.TEST_AK6)
 
+  before( () => {
+    sinon.stub(siigoFile, 'wizardsiigofile').returns(Promise.resolve('mockToken'))
+    sinon.stub(gitmanager, 'getProjects').returns(Promise.resolve({
+      Architecture:'55db46d7-c3a3-481c-b4a2-14c9f75e547a', 
+      Siigo:'2b375626-f976-487e-9aa8-097804b773cc'
+    }))
+  })
+
   it('Generates a project', () => {
 
     const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'Siigo.Test.Ak6'))
@@ -30,8 +42,12 @@ describe('siigo:test.ak6', () => {
       .then(() => {
         assert.file([
           '.babelrc',
-          '.gitignore']);
-        assert.equal(true, shelljs.exec('yarn audit --level critical', {silent: true}).code < YARN_AUDIT_CODE.CRITICAL)
+          '.gitignore'
+        ]);
+        if(!runningOnAzurePipeline()){
+          const auditCommand = shelljs.exec('yarn audit --level critical', { silent: true})
+          assert.equal(true, auditCommand.code < YARN_AUDIT_CODE.CRITICAL, `The archetype has dependencies with bugs. \n${auditCommand.stderr}`)
+        }
       });
   });
 
@@ -54,5 +70,9 @@ describe('siigo:test.ak6', () => {
           '.gitignore']);
       });
   });
+
+  after(() => {
+    sinon.restore()
+  })
 });
 
