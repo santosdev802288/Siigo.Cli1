278f3c2ef00a2e92c48b95abf06f38c33430ec01
Francisco Anacona <anac800776>
2022-03-09 09:03:51 -0500

    add new libraries go

--- src/generators/bolt/index.ts
+++ src/generators/bolt/index.ts
@@ -5,10 +5,12 @@ import {siigosay} from'@nodesiigo/siigosay'
 import {MicroserviceGenerator} from '../../utils/generator/microservice'
 import { getAllParametersSiigo, wizardsiigofile } from '../../utils/siigoFile'
 import { saveStatistic } from '../../utils/statistics/statistic'
+import shell from 'shelljs'
+
 
 export default class GolangMSGenerator extends MicroserviceGenerator {
 
-    appConfig: { description?: any; author?: any; name?: any; token?: any; auth?: any; email?: any } = {}
+    appConfig: { description?: any; author?: any; name?: any; token?: any; auth?: any; redis?: any; email?: any } = {}
 
     constructor(args: any, opt: any) {
         super(args, opt)
@@ -39,6 +41,7 @@ export default class GolangMSGenerator extends MicroserviceGenerator {
             type: String
         })
     }
+
 
     async initializing(){
         this.log(siigosay('Siigo Generator Golang.'))
@@ -82,6 +85,14 @@ export default class GolangMSGenerator extends MicroserviceGenerator {
                 message: 'Do you want to add the module SECURITY?'
             }
         ]);
+
+        const respRedis = await this.prompt([
+            {
+                type: 'confirm',
+                name: 'redis',
+                message: 'Do you want to add the module REDIS?'
+            }
+        ]);
         
         this.answers = await this.prompt([
             {
@@ -90,7 +101,10 @@ export default class GolangMSGenerator extends MicroserviceGenerator {
                 message: 'Is the configuration correct?'
             }
         ]);
-        this.appConfig.auth =  response.auth;
+
+        this.appConfig.auth =  response.auth;      
+        this.appConfig.redis =  respRedis.redis; 
+
         if (!this.answers.ready)
             this.cancelCancellableTasks()
     }
@@ -119,15 +133,40 @@ export default class GolangMSGenerator extends MicroserviceGenerator {
         this.fs.copy( this.templatePath('.dots/.*'), this.destinationRoot(),{},{config: this.appConfig});
         this.fs.copy(this.templatePath('.dots/.**/**'),this.destinationRoot(),{},{config: this.appConfig})
 
-        if(this.appConfig.auth){
-            this.fs.delete('src/boot/boot.go')
-            this.fs.delete('go.mod')
-            this.fs.delete('go.sum')
-
-            this.fs.copyTpl(this.templatePath('.mod_auth/boot'),this.destinationPath('src/boot/'),{config: this.appConfig})
-            this.fs.copyTpl(this.templatePath('.mod_auth/_go.mod'),this.destinationPath('go.mod'),{config: this.appConfig})
-            this.fs.copyTpl(this.templatePath('.mod_auth/_go.sum'),this.destinationPath('go.sum'),{config: this.appConfig})
+        if(this.appConfig.auth && this.appConfig.redis){
+            this._copyFilesModules('mod_auth_redis',['boot','config','fxmodule']);
+        }else{
+            if(this.appConfig.auth){
+                this._copyFilesModules('mod_auth',['boot']);
+            }
+            if(this.appConfig.redis){
+                this._copyFilesModules('mod_redis',['config','fxmodule']);
+            }
         }
+    }
+
+    _copyFilesModules(path:string , files:string[]){
+        files.forEach(file => {
+            this.fs.copyTpl(this.templatePath(`.${path}/${file}`),this.destinationPath(`src/${file}/`),{config: this.appConfig})
+        })
+    }
+
+    install(){
+        if(this.appConfig.auth && this.appConfig.redis){
+            shell.exec('go get dev.azure.com/SiigoDevOps/Siigo/_git/Siigo.Golang.Security.git/Interceptor')
+            shell.exec('go get github.com/grpc-ecosystem/go-grpc-middleware/auth')
+            shell.exec('go get dev.azure.com/SiigoDevOps/Siigo/_git/Siigo.Golang.DistributedCache.git/Redis')
+        }else{
+            if(this.appConfig.auth){
+                shell.exec('go get dev.azure.com/SiigoDevOps/Siigo/_git/Siigo.Golang.Security.git/Interceptor')
+                shell.exec('go get github.com/grpc-ecosystem/go-grpc-middleware/auth')
+            }
+            if(this.appConfig.redis){
+                shell.exec('go get dev.azure.com/SiigoDevOps/Siigo/_git/Siigo.Golang.DistributedCache.git/Redis')
+            }
+            shell.exec('go mod tidy')
+        }
+        
     }
 
     end(): void {
