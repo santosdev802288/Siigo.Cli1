a291a6313bcd9b68851c5a9dbb357230da7353ff
Francisco Anacona <anac800776>
2022-04-02 15:40:12 -0500

    add unit test and exceptionmiddleware

--- src/generators/core/templates/Siigo.Microservice.Infrastructure/Extensions/ClassExtensions.cs
+++ src/generators/core/templates/Siigo.Microservice.Infrastructure/Extensions/ClassExtensions.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using <%= config.projectPrefix %>.<%= config.nameCapitalize %>.Domain.Exception;
 
 namespace <%= config.projectPrefix %>.<%= config.nameCapitalize %>.Infrastructure.Extensions
@@ -55,6 +58,34 @@ namespace <%= config.projectPrefix %>.<%= config.nameCapitalize %>.Infrastructur
                 throw new NotFoundException(message);
             }
             return number;
+        }
+
+                public static IEnumerable<TSource> FromHierarchy<TSource>(
+            this TSource source,
+            Func<TSource, TSource> nextItem,
+            Func<TSource, bool> canContinue)
+        {
+            for (TSource current = source; canContinue(current); current = nextItem(current))
+            {
+                yield return current;
+            }
+        }
+
+        public static IEnumerable<TSource> FromHierarchy<TSource>(
+            this TSource source,
+            Func<TSource, TSource> nextItem)
+            where TSource : class
+            => FromHierarchy(source, nextItem, s => s != null);
+
+        /// <summary>
+        /// Get All InnerException Messages in a single string
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public static string GetAllMessages(this Exception exception)
+        {
+            var messages = exception.FromHierarchy(ex => ex.InnerException).Select(ex => ex.Message);
+            return string.Join($"{Environment.NewLine}   ", messages);
         }
     }
 }
--- test/generator.factory.ts
+++ test/generator.factory.ts
@@ -1,17 +1,19 @@
 import path from 'path'
-import { Constructor, RunContextSettings } from 'yeoman-test';
-import Generator from 'yeoman-generator';
+import { Constructor, RunContextSettings } from 'yeoman-test'
+import Generator from 'yeoman-generator'
 
 import CicdGenerator from '../src/generators/cicd'
 import NodeMSGenerator from '../src/generators/node'
+import CoreMSGenerator from '../src/generators/core'
 import Ak6TestingGenerator from '../src/generators/test-ak6'
 import DatadogGenerator from '../src/generators/datadog'
-import MigrationGenerator from '../src/generators/skub';
-import DotnetMSGenerator from '../src/generators/library';
+import MigrationGenerator from '../src/generators/skub'
+import DotnetMSGenerator from '../src/generators/library'
 
 export enum SiigoGenerator {
   LIBRARY,
   MS_CICD,
+  MS_CORE,
   MS_NODE,
   TEST_AK6,
   TEST_DD,
@@ -41,6 +43,14 @@ export function getGenerator(generator: SiigoGenerator): TestGenerator {
           namespace: 'siigo:cicd'
         }
       }
+      case SiigoGenerator.MS_CORE:
+        return {
+            generatorOrNamespace: CoreMSGenerator as Constructor<Generator>,
+            settings: {
+                resolved: path.join(__dirname, '../src/generators/core', 'index.js'),
+                namespace: 'siigo:core'
+            }
+        }
     case SiigoGenerator.MS_NODE:
       return {
         generatorOrNamespace: NodeMSGenerator as Constructor<Generator>,
